6f29fd3254bc638e9a8df8b598510bcc28bbf4ad
Russ <russ@zeamster.com>
2016-11-17 16:22:41 -0500

    php7 fix

--- tests/init-composer.sh
+++ tests/init-composer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo php5dismod xdebug
+sudo phpdismod xdebug
 # install Composer
 curl -s https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
